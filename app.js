e31a1fe1d5b4b756995372d72a1241e565e2066d
hossein <hossein838485@gmail.com>
2022-03-25 20:20:10 +0430

    debug and finih app.js

--- app.js
+++ app.js
@@ -1,49 +1,134 @@
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
+
 let random1 = Math.floor(randomNumber(10, 19));
-let random2 = Math.floor(randomNumber(10, 19));
+let random2 = Math.floor(randomNumber(20, 29));
 let random3 = Math.floor(randomNumber(30, 39));
-let random4 = Math.floor(randomNumber(30, 39));
+let random3_2 = Math.floor(randomNumber(30, 39));
+let random4 = Math.floor(randomNumber(40, 49));
 let random5 = Math.floor(randomNumber(50, 59));
-let random6 = Math.floor(randomNumber(50, 59));
+let random5_2 = Math.floor(randomNumber(50, 59));
+let random6 = Math.floor(randomNumber(60, 69));
 let random7 = Math.floor(randomNumber(70, 79));
-let random8 = Math.floor(randomNumber(60, 69));
-let random9 = Math.floor(randomNumber(40, 51));
-let random10 = Math.floor(randomNumber(40, 53));
+let random8 = Math.floor(randomNumber(80, 89));
+let random9 = Math.floor(randomNumber(90, 99));
 
 function Barrier() {
-  square[random1].classList.remove("Barrier");
-  square[random2].classList.remove("Barrier");
-  square[random3].classList.remove("Barrier");
-  square[random4].classList.remove("Barrier");
-  square[random5].classList.remove("Barrier");
-  square[random6].classList.remove("Barrier");
-  square[random7].classList.remove("Barrier");
-  square[random8].classList.remove("Barrier");
-  square[random9].classList.remove("Barrier");
-  square[random8].classList.remove("Barrier");
+  function randomOne() {
+    if (random1 < 19) {
+      square[random1].classList.remove("Barrier");
+      random1++;
+      square[random1].classList.add("Barrier");
+    } else{
+      square[random1].classList.remove("Barrier");
+      random1 -= 10;
+    }
+  }
+  function randomTwo() {
+    if (random2 < 29) {
+      square[random2].classList.remove("Barrier");
+      random2++;
+      square[random2].classList.add("Barrier");
+    } else{
+      square[random2].classList.remove("Barrier");
+      random2 -= 10;
+    }
+  }
+  function randomThree() {
+    if (random3 < 39) {
+      square[random3].classList.remove("Barrier");
+      square[random3_2].classList.remove("Barrier");
+      random3++;
+      random3_2++;
+      square[random3].classList.add("Barrier");
+      square[random3_2].classList.add("Barrier");
 
-  random1++;
-  random2++;
-  random3++;
-  random4++;
-  random5++;
-  random6++;
-  random7++;
-  random8++;
-  random9++;
-  random10++;
+    } else{
+      square[random3].classList.remove("Barrier");
+      square[random3_2].classList.remove("Barrier");
+      random3_2 -= 10
+      random3 -= 10;
+    }
+  }
+  function randomFour() {
+    if (random4 < 49) {
+      square[random4].classList.remove("Barrier");
+      random4++;
+      square[random4].classList.add("Barrier");
+    } else{
+      square[random4].classList.remove("Barrier");
+      random4 -= 10;
+    }
+  }
+  function randomFive() {
+    if (random5 < 59) {
+      square[random5].classList.remove("Barrier");
+      square[random5_2].classList.remove("Barrier");
 
-  square[random1].classList.add("Barrier");
-  square[random2].classList.add("Barrier");
-  square[random3].classList.add("Barrier");
-  square[random4].classList.add("Barrier");
-  square[random5].classList.add("Barrier");
-  square[random6].classList.add("Barrier");
-  square[random7].classList.add("Barrier");
-  square[random8].classList.add("Barrier");
-  square[random9].classList.add("Barrier");
-  square[random10].classList.add("Barrier");
+      random5++;
+      random5_2++;
+
+      square[random5].classList.add("Barrier");
+      square[random5_2].classList.add("Barrier");
+
+    } else{
+      square[random5].classList.remove("Barrier");
+      square[random5_2].classList.remove("Barrier");
+      random5_2 -= 10
+      random5 -= 10;
+    }
+  }
+  function randomSix() {
+    if (random6 < 69) {
+      square[random6].classList.remove("Barrier");
+      random6++;
+      square[random6].classList.add("Barrier");
+    } else{
+      square[random6].classList.remove("Barrier");
+      random6 -= 10;
+    }
+  }
+  function randomSeven() {
+    if (random7 < 79) {
+      square[random7].classList.remove("Barrier");
+      random7++;
+      square[random7].classList.add("Barrier");
+    } else{
+      square[random7].classList.remove("Barrier");
+      random7 -= 10;
+    }
+  }
+  function randomEighit() {
+    if (random8 < 89) {
+      square[random8].classList.remove("Barrier");
+      random8++;
+      square[random8].classList.add("Barrier");
+    } else{
+      square[random8].classList.remove("Barrier");
+      random8 -= 10;
+    }
+  }
+  function randomNine() {
+    if (random9 < 89) {
+      square[random9].classList.remove("Barrier");
+      random9++;
+      square[random9].classList.add("Barrier");
+    } else{
+      square[random9].classList.remove("Barrier");
+      random9 -= 10;
+    }
+  }
+
+  randomOne()
+  randomTwo()
+  randomThree()
+  randomFour()
+  randomFive()
+  randomSix()
+  randomSeven()
+  randomEighit()
+  randomNine()
+  
 }
 setInterval(Barrier, 650);
